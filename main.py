c7526431bb7b2aaf38e5b9db0c995fb0cf226cca
Szymon Roszak <szymonroszak@icloud.com>
2022-04-27 17:26:09 +0200

    poprawiona wersja

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import math
 liczba_figur=int(input())
-for i in range(liczba_figur):
+suma=0
+for i in range(0, liczba_figur):
     dane_figury=input().split(" ")
-    suma=0
     if len(dane_figury)==3:
-        polowa_obwodu=1/2*(float(dane_figury[0])|+float(dane_figury[1])+float(dane_figury[2]))
-        pole_trojkata=math.sqrt(polowa_obwodu * (polowa_obwodu-dane_figury[0]) * (polowa_obwodu-dane_figury[1]) * (polowa_obwodu-dane_figury[2]))
+        polowa_obwodu=1/2 * (float(dane_figury[0])+float(dane_figury[1])+float(dane_figury[2]))
+        pole_trojkata=math.sqrt(polowa_obwodu * (polowa_obwodu-float(dane_figury[0])) * (polowa_obwodu-float(dane_figury[1])) * (polowa_obwodu-float(dane_figury[2])))
         suma+=pole_trojkata
     elif len(dane_figury)==2:
         pole_prostokata=float(dane_figury[0])*float(dane_figury[1])
@@ -15,4 +15,4 @@ for i in range(liczba_figur):
         suma+=pole_kola
     else:
         print("Błąd: można podać maksymalnie 3 liczby")
-    print(round(suma,2))
+print(round(suma,2))
